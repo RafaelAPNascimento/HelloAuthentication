04a287da1ca694de117455e03def19381dbf8505
RafaelAPNascimento <rfx.go.on@gmail.com>
2021-05-12 19:24:48 -0300

    k8 dpeloyment

--- src/test/java/util/Constants.java
+++ src/test/java/util/Constants.java
@@ -2,6 +2,7 @@ package util;
 
 public class Constants {
 
-    public static final String BASE_URI = "http://localhost:8989/hello-authentication/api";
+    public static final String BASE_URI = "http://192.168.49.2:30001/hello-authentication/api";
+    //public static final String BASE_URI = "http://localhost:8089/hello-authentication/api";
     public final static String TOKEN_HEADER = "{\"alg\":\"HS256\", \"typ\":\"jwt\"}";
 }
